66925a570061fa15e3c404ef8e642129dac8caac
HansiHE <hansihe@hansihe.com>
2013-07-31 03:51:10 +0200

    added js

---
+++ molecular.js
@@ -0,0 +1,15 @@
+(function() {
+
+    Molecular = {};
+
+    Molecular.register = function(name, func) {
+        if(to_load.indexOf(name) > -1) {
+            func();
+        }
+    };
+
+    if(document.head.getAttribute("data-molecules")) {
+        var to_load = document.head.getAttribute("data-molecules").split(" ");
+    }
+
+})();
